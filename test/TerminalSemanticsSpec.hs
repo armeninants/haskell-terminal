fc1c75fc4155f0fd001fd396947c3f6e5a7fa4cc
Armen Inants <armen@inants.com>
2021-06-22 14:59:42 +0200

    Removed unused test

---
+++ test/TerminalSemanticsSpec.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+module TerminalSemanticsSpec (spec) where
+
+import Import
+import TerminalSemantics
+import Test.Hspec
+import Test.Hspec.QuickCheck
+
+
+
+
+
+spec :: Spec
+spec = do
+    return ()
+--   describe "plus2" $ do
+--     it "basic check" $ plus2 0 `shouldBe` 2
+--     it "overflow" $ plus2 maxBound `shouldBe` minBound + 1
+--     prop "minus 2" $ \i -> plus2 i - 2 `shouldBe` i
