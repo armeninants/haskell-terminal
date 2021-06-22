7ba1409e050c5c8b018e000f6eb91a86721dcf09
Armen Inants <armen@inants.com>
2021-06-22 17:39:28 +0200

    Added a test

--- test/TerminalSemanticsSpec.hs
+++ test/TerminalSemanticsSpec.hs
@@ -26,23 +26,21 @@ executeMany clis = newApp >>= flip runRIO (forM clis execCliString)
 cmd1 :: String
 cmd1 = "echo \"abc\" | cat"
 
-cmd2 :: String
-cmd2 = "export varName=\"aaaa\""
+cmds2 :: [String]
+cmds2 = ["export varName=\"aaaa\"", "export otherVar=\"bbb$varName\"", "echo \"$otherVar\""]
 
 cmd3 :: String
-cmd3 = "export otherVar=\"bbb$varName\""
+cmd3 = "grep \"e\" \"test-data/file1.txt\" | wc"
 
 cmd4 :: String
-cmd4 = "echo \"$otherVar\""
+cmd4 = "grep ve \"test-data/file1.txt\" -A 1 -B 1"
 
 cmd5 :: String
-cmd5 = "grep \"e\" \"test-data/file1.txt\" | wc"
+cmd5 = "shell ls | grep test-data"
 
-cmd6 :: String
-cmd6 = "grep ve \"test-data/file1.txt\" -A 1 -B 1"
+cmds6 :: [String]
+cmds6 = ["export echoVar=echo", "$echoVar \"echo this\""]
 
-cmd7 :: String
-cmd7 = "shell ls | grep test-data"
 
 spec :: Spec
 spec = do
@@ -50,12 +48,12 @@ spec = do
         it "echo passes output to cat" $
             execute cmd1 `shouldReturn` Success "abc"
         it "variable exporting and interpolation" $
-            executeMany [cmd2,cmd3,cmd4] `shouldReturn` [Success "", Success "", Success "bbbaaaa"]
+            executeMany cmds2 `shouldReturn` [Success "", Success "", Success "bbbaaaa"]
         it "checking grep and wc" $
-            execute cmd5 `shouldReturn` Success "5"
+            execute cmd3 `shouldReturn` Success "5"
         it "grep flags -A and -B work properly" $
-            execute cmd6 `shouldReturn` Success "four\nfive\nsix\n--\nsix\nseven\neight\n"
+            execute cmd4 `shouldReturn` Success "four\nfive\nsix\n--\nsix\nseven\neight\n"
         it "shell works" $
-            execute cmd7 `shouldReturn` Success "test-data\n"
-
-
+            execute cmd5 `shouldReturn` Success "test-data\n"
+        it "commands can be interpolated" $
+            executeMany cmds6 `shouldReturn` [Success "", Success "echo this"]
